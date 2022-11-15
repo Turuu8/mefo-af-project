5a7933324e39ef9357f8a69082f97a176f017343
mairadavaa <davaamaira@gmail.com>
2022-11-15 12:51:48 +0800

    ...home

--- frontend/src/components/detail/target.js
+++ frontend/src/components/detail/target.js
@@ -1,34 +1,41 @@
-import part1 from "../../assets/images/301610468_1226762367869520_4778822227424414284_n.jpg";
-import part2 from "../../assets/images/301676391_760146185313384_7155529653519959105_n.jpg";
-import part3 from "../../assets/images/301815609_782199779572010_88340814944985780_n.jpg";
-import part4 from "../../assets/images/302495965_171425098737929_6807918357025679645_n.jpg";
+
+import image6 from "../../assets/images/clothes men/6.png";
+import image10 from "../../assets/images/clothes men/10.png";
+import image9 from "../../assets/images/clothes men/9.png";
+import image7 from "../../assets/images/clothes men/7.png";
+import image5 from "../../assets/images/clothes men/5.png"
 import { forwardRef, useImperativeHandle, useRef } from "react";
 import classes from "../../assets/styles/home.module.scss"
 export const Target = forwardRef((props, ref) => {
   const div1 = useRef(null);
-  const div2 = useRef(null);
-  const div3 = useRef(null);
-  const div4 = useRef(null);
+  // const div2 = useRef(null);
+  // const div3 = useRef(null);
+  // const div4 = useRef(null);
+  // const div5 = useRef(null);
   useImperativeHandle(ref, () => ({
     div1,
-    div2,
-    div3,
-    div4,
+    // div2,
+    // div3,
+    // div4,
+    // div5,
   }));
   return (
     <>
       <div ref={div1}>
-          <img src={part4} className={classes.home_img} alt="1"/>
+          <img src={image6} className={classes.home_img} alt="1"/>
       </div>
-      <div ref={div2}>
-          <img src={part1} className={classes.home_img} alt ="2"/>
+      {/* <div ref={div2}>
+          <img src={image10} className={classes.home_img} alt ="2"/>
       </div>
       <div ref={div3}>
-          <img src={part2} className={classes.home_img} alt ="2"/>
+          <img src={image9} className={classes.home_img} alt ="3"/>
       </div>
       <div ref={div4}>
-          <img src={part3} className={classes.home_img} alt ="2"/>
+          <img src={image7} className={classes.home_img} alt ="4"/>
       </div>
+      <div ref={div4}>
+          <img src={image5} className={classes.home_img} alt ="5"/>
+      </div> */}
     </>
   );
 });
--- frontend/src/pages/main/home.jsx
+++ frontend/src/pages/main/home.jsx
@@ -2,9 +2,10 @@ import { useContext } from "react";
 import { Login, Signup, ForgotPassword } from "../../pages";
 import { GlobalContext } from "../../context/GlobalContext";
 import classes from "../../assets/styles/home.module.scss";
-// import { Target } from "../../components/detail/target";
-// import { Controller, Scene } from "react-scrollmagic";
-// import { Timeline, Tween } from "react-gsap";
+
+import { Target } from "../../components/detail/target";
+import { Controller, Scene } from "react-scrollmagic";
+import { Timeline, Tween } from "react-gsap";
 export const Home = () => {
   const {
     loginOpen: { isLoginOpen },
@@ -14,9 +15,113 @@ export const Home = () => {
   return (
     <>
       <div className={classes.home}>
-        <div className={classes.home_text1}> A New Look at Fashion of the MEPO AF</div>
-        <div className={classes.home_text2}> EXCLUSIVE COLLECTIONS OF POPULAR BRANDS ABOUT HUMAN CULTURE</div>
-        {/* <div className={classes.home_text2}> BRANDS ABOUT HUMAN CULTURE</div> */}
+        <div className={classes.home_text1}>
+          {" "}
+          A New Look at Fashion of the MEPO AF
+        </div>
+        <div className={classes.home_text2}>
+          {" "}
+          EXCLUSIVE COLLECTIONS OF POPULAR BRANDS ABOUT HUMAN CULTURE
+        </div>
+        <div className={classes.home_img}></div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "10px",
+          }}
+        >
+          ---
+        </div>
+        <div className={classes.home_text2}> Our collections</div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            marginTop: "30px",
+            position: "relative",
+          }}
+        >
+          <div className="hello">
+            <Controller>
+              <Scene
+                duration={2000}
+                pin={".hello"}
+                indicators={true}
+                triggerHook={2}
+              >
+                {(progress) => (
+                  <Timeline totalProgress={progress} paused target={<Target />}>
+                    <Tween
+                      from={{
+                        position: "absolute",
+                        left: "0%",
+                        top: "0",
+                      }}
+                      to={{
+                        position: "absolute",
+                        left: "38.5%",
+                        top: "0",
+                        transform: `scale(1.5)`,
+                      }}
+                      target="div1"
+                      position="0"
+                      duration={1}
+                    />
+                    {/* <Tween
+                      from={{
+                        position: "absolute",
+                        right: "20%",
+                        top: "3020px",
+                      }}
+                      to={{
+                        position: "absolute",
+                        right: "54%",
+                        top: "3235px",
+                        transform: `scale(1.5)`,
+                      }}
+                      target="div2"
+                      position="0"
+                      duration={1}
+                    />
+                    <Tween
+                      from={{
+                        position: "absolute",
+                        right: "20%",
+                        top: "3400px",
+                      }}
+                      to={{
+                        position: "absolute",
+                        right: "54%",
+                        top: "3553px",
+                        transform: `scale(1.5)`,
+                      }}
+                      target="div3"
+                      position="0"
+                      duration={1}
+                    />
+                    <Tween
+                      from={{
+                        position: "absolute",
+                        left: "20%",
+                        top: "3400px",
+                      }}
+                      to={{
+                        position: "absolute",
+                        left: "38.5%",
+                        top: "3553px",
+                        transform: `scale(1.5)`,
+                      }}
+                      target="div4"
+                      position="0"
+                      duration={1}
+                    /> */}
+                  </Timeline>
+                )}
+              </Scene>
+            </Controller>
+          </div>
+        </div>
       </div>
       {isLoginOpen && <Login />}
       {isSignupOpen && <Signup />}
