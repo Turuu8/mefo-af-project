f6f89bac213812cf7129ef633bb64f332aafa8a3
unknown <amka.storm9812@gmail.com>
2022-11-18 01:04:22 +0800

    Finished order

--- backend/models/orderModel.ts
+++ backend/models/orderModel.ts
@@ -1,5 +1,27 @@
+import { IOrder } from "../utils/types";
 import { Schema, model, Types } from "mongoose";
 
-const OrderSchema = new Schema({}, { timestamps: true });
+enum ETypeOrder {
+  ORDERED = "ORDERED",
+  COMPLETED = "COMPLETED",
+  DELIVERING = "DELIVERING",
+}
 
-export const OrderModel = model("Order", OrderSchema);
+const OrderSchema = new Schema(
+  {
+    user: { type: Types.ObjectId, ref: "User" },
+    fullname: { type: String, required: true },
+    email: String,
+    address: { type: Types.ObjectId, required: "Address" },
+    orderItems: [{ type: Types.ObjectId, ref: "Product" }],
+    orderStatus: {
+      type: String,
+      default: ETypeOrder.ORDERED,
+      enum: Object.values(ETypeOrder),
+    },
+    paymentID: { type: String },
+  },
+  { timestamps: true }
+);
+
+export const OrderModel = model<IOrder>("Order", OrderSchema);
--- backend/utils/findDocument.ts
+++ backend/utils/findDocument.ts
@@ -8,7 +8,7 @@ import { CategoryModel } from "../models/categoryModel";
 export const findUserByKey = async (payload: Object) => {
   return await UserModel.findOne(payload);
 };
-export const findUserByID = async (payload: string) => {
+export const findUserByID = async (payload: string | undefined) => {
   return await UserModel.findById(payload);
 };
 export const updateUserByID = async (id: string | undefined, data: object) => {
@@ -32,12 +32,14 @@ export const findCategoryByKey = async (payload: Object) => {
 export const findProductByKey = async (payload: object) => {
   return await ProductModel.findOne(payload);
 };
-export const findProductByID = async (payload: string) => {
+export const findProductByID = async (payload: string | undefined) => {
   return await ProductModel.findById(payload);
 };
 
 //Address
-
+export const findAddressByKey = async (payload: object) => {
+  return await AddressModel.findOne(payload);
+};
 export const updateAddressByKey = async (id: object, data: object) => {
   return await AddressModel.findOneAndUpdate(id, data);
 };
