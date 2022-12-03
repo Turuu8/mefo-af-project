d925159ac584ef9284b10fb90930814b86a48979
unknown <amka.storm9812@gmail.com>
2022-12-03 13:45:27 +0800

    added designer's name

--- frontend/src/components/layouts/Footer.jsx
+++ frontend/src/components/layouts/Footer.jsx
@@ -12,34 +12,42 @@ export const Footer = () => {
     language: { lang },
   } = useContext(GlobalContext);
   return (
-    <div className={classes.footer}>
-      {Object.entries(langFooterArr[lang]).map(([name, arr]) => (
-        <div key={name} className={classes.footer_item}>
-          <div className={classes.footer_item_inner}>
-            <h4>{name}</h4>
-            <div>
-              {arr.map((el, i) => (
-                <Link onClick={el.name === "Log out" || el.name === "Гарах" ? logout : null} key={i} to={el.path}>
-                  {el.name}
-                </Link>
-              ))}
+    <>
+      <div className={classes.footer}>
+        {Object.entries(langFooterArr[lang]).map(([name, arr]) => (
+          <div key={name} className={classes.footer_item}>
+            <div className={classes.footer_item_inner}>
+              <h4>{name}</h4>
+              <div>
+                {arr.map((el, i) => (
+                  <Link onClick={el.name === "Log out" || el.name === "Гарах" ? logout : null} key={i} to={el.path}>
+                    {el.name}
+                  </Link>
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-      <div className={classes.footer_item}>
-        <div className={classes.footer_item_inner}>
-          <h4>{lang === "en" ? "SOCIAL LINKS" : "Сошиал хаяг"}</h4>
-          <span>
-            <a href="www.instagram.com">
-              <BsInstagram size={22} />
-            </a>
-            <a href="www.facebook.com">
-              <BsFacebook size={22} />
-            </a>
-          </span>
+        ))}
+        <div className={classes.footer_item}>
+          <div className={classes.footer_item_inner}>
+            <h4>{lang === "en" ? "SOCIAL LINKS" : "Сошиал хаяг"}</h4>
+            <span>
+              <a href="www.instagram.com">
+                <BsInstagram size={22} />
+              </a>
+              <a href="www.facebook.com">
+                <BsFacebook size={22} />
+              </a>
+            </span>
+          </div>
         </div>
       </div>
-    </div>
+      <div className={classes.footerBottom}>
+        <Link to={"/"} className={classes.footer_image_container}>
+          <img alt="mepoAfLogo" src="https://res.cloudinary.com/mustnest/image/upload/v1669191520/Mepo_Af/logoBlack_awmpvg.png" />
+        </Link>
+        <span>@2022 Mepo Af. Website designed by Surneke</span>
+      </div>
+    </>
   );
 };
