64920de8bd2aae7294c6af81c8cb48a2d2077913
unknown <amka.storm9812@gmail.com>
2022-12-05 19:21:31 +0800

    No commit

--- frontend/src/components/bag/Build/Order/Order.jsx
+++ frontend/src/components/bag/Build/Order/Order.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import clx from "classnames";
 import { useState } from "react";
-import DropDown from "./DropDown";
+import DropDown from "./Dropdown";
 import motion from "framer-motion";
 import close_btn from "../../../../assets/images/close-black.svg";
 import css from "../../../../assets/styles/Bag/OrderStyle.module.scss";
@@ -9,7 +9,7 @@ import arrow_down from "../../../../assets/images/arrow-down-black.svg";
 
 const Order = () => {
   const [sizeOpen, setSizeOpen] = useState(false);
-  const [quantity, setQuantity] = useState("");
+  const [quantity, setQuantity] = useState("1");
   const [size, setSize] = useState("XXL");
   const quantityFuction = (number) => {
     if (number === 0 || number < 0) return;
@@ -30,30 +30,14 @@ const Order = () => {
           <div className={css.content}>
             <p>Black</p>{" "}
             <div className={css.sizeAndQuantity__dropdown}>
-              <button
-                className={css.dropdown_btn}
-                onClick={() => setSizeOpen((old) => !old)}
-              >
+              <button className={css.dropdown_btn} onClick={() => setSizeOpen((old) => !old)}>
                 <p>{size}</p>
-                <img
-                  className={clx(sizeOpen && css.rotateIcon)}
-                  src={arrow_down}
-                  alt="arrow down"
-                />
+                <img className={clx(sizeOpen && css.rotateIcon)} src={arrow_down} alt="arrow down" />
               </button>
-              <DropDown
-                change={setSize}
-                open={sizeOpen}
-                setOpen={setSizeOpen}
-              />
+              <DropDown change={setSize} open={sizeOpen} setOpen={setSizeOpen} />
             </div>
             <div className={css.sizeAndQuantity__dropdown}>
-              <input
-                type="number"
-                className={css.dropdown_btn}
-                value={quantity}
-                onChange={(e) => quantityFuction(e.target.value)}
-              />
+              <input type="number" className={css.dropdown_btn} value={quantity} onChange={(e) => quantityFuction(e.target.value)} />
             </div>
             <p>USD 650.00</p>
             <button className={css.removeBtn}>Remove</button>
@@ -68,26 +52,14 @@ const Order = () => {
           <p>USD 650.00 | Black</p>
           <div className={css.sizeAndQuantity}>
             <div className={css.sizeAndQuantity__dropdown}>
-              <button
-                className={css.dropdown_btn}
-                onClick={() => setSizeOpen((old) => !old)}
-              >
+              <button className={css.dropdown_btn} onClick={() => setSizeOpen((old) => !old)}>
                 <p>{size}</p>
                 <img src={arrow_down} alt="arrow down" />
               </button>
-              <DropDown
-                change={setSize}
-                open={sizeOpen}
-                setOpen={setSizeOpen}
-              />
+              <DropDown change={setSize} open={sizeOpen} setOpen={setSizeOpen} />
             </div>
             <div className={css.sizeAndQuantity__dropdown}>
-              <input
-                type="number"
-                className={css.dropdown_btn}
-                value={quantity}
-                onChange={(e) => quantityFuction(e.target.value)}
-              />
+              <input type="number" min={1} className={css.dropdown_btn} value={quantity} onChange={(e) => quantityFuction(e.target.value)} />
             </div>
           </div>
         </div>
